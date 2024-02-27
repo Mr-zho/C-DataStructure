20d0bd42f35d8e5bcdb606add61bdfb767b51dab
wenliang <wenliang@163.com>
2024-02-27 18:02:28 +0800

    [新增] 使用正则判断用户名合法性

--- project-Contact/contactMenu.c
+++ project-Contact/contactMenu.c
@@ -1,5 +1,9 @@
 #include "contactMenu.h"
 #include <stdio.h>
+#include <regex.h>
+#include <string.h>
+
+#define BUFFER_SIZE  16
 
 /* 枚举数据类型的空间占用 一定是4个字节 */
 enum STATUS_CODE
@@ -8,6 +12,7 @@ enum STATUS_CODE
     NULL_PRT,
     MALLOC_ERROR,
     INVALID_ACCESS,
+    ILLEGALITY,
 };
 
 /* 菜单 */
@@ -22,13 +27,102 @@ int contactMenu(int optionLight)
     return ON_SUCCESS;
 }
 
+static int checkUserNameLegal(const char * name)
+{
+    if (name == NULL)
+    {
+        return NULL_PRT;
+    }
+
+    int userNameLength = strlen(name);
+    if (userNameLength < 3 || userNameLength > 15)
+    {
+        return ILLEGALITY;
+    }
+
+    const char * tmpName = name;
+    #if 0
+    /* 方案1 */
+    while (*tmpName != '\0')
+    {
+        if (*tmpName == '~' || *tmpName == '!' || *tmpName == '@')
+        {
+            return ILLEGALITY;
+        }
+        tmpName++;
+    }
+    return ON_SUCCESS;
+    #else
+    /* 方案2 */
+    regex_t regex;
+    memset(&regex, 0, sizeof(regex));
+
+    char * pattern = "[!@$]";
+
+    /* 编译正则表达式 */
+    int ret = regcomp(&regex, pattern, 0);
+    if (ret)
+    {
+        printf("regcomp error");
+        return ret;
+    }
+
+    /* 执行正则表达式 */
+    ret = regexec(&regex, name, 0, NULL, 0);
+    regfree(&regex);
+
+    if (ret == 0)
+    {
+        printf("存在非法字符\n");
+        return ILLEGALITY;
+    }
+    else if (ret == REG_NOMATCH)
+    {
+        /* 不存在非法字符 */
+        return ON_SUCCESS;
+    }
+    else
+    {
+        printf("正则匹配失败\n");
+        return ILLEGALITY;
+    }
+    #endif
+}
+
 /* 添加联系人 */
 int addPerson()
 {
-    /* */
-    printf("\033[31;47mgfgghtynt联系人  \033[0m\n");
-    printf("\033[31;47mfergtrgtrg 删除联系人  \033[0m\n");
-    printf("\033[31;47mfregthg5t联系人  \033[0m\n");
+    #if 0
+    printf("\033[31;47m-----------------------------\033[0m\n");
+    printf("\033[31;47m                             \033[0m\n");
+    printf("\033[31;47m                             \033[0m\n");
+    printf("\033[31;47m                             \033[0m\n");
+    printf("\033[31;47m                             \033[0m\n");
+    printf("\033[31;47m                             \033[0m\n");
+    printf("\033[31;47m                             \033[0m\n");
+    printf("\033[31;47m                             \033[0m\n");
+    printf("\033[31;47m                             \033[0m\n");
+    printf("\033[31;47m-----------------------------\033[0m\n");
+    #endif
+
+
+    char userName[BUFFER_SIZE] = { 0 };
+    printf("请输入用户名:");
+    scanf("%s", userName);
+
+    int ret = checkUserNameLegal(userName);
+    if (ret != ON_SUCCESS)
+    {
+        printf("添加联系人失败\n");
+        getchar();
+        getchar();
+        return ret;
+    }
+
+
+  
+
 
     getchar();
+    printf("添加联系人成功");
 }
